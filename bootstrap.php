ccd7bb4fd991258fbfb7ebca8f9e151e7fd38a92
rvogel <vogel@hallowelt.biz>
2022-01-10 16:36:43 +0100

    Use `ComponentLoader`

--- bootstrap.php
+++ bootstrap.php
@@ -6,32 +6,35 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_RUNJOBSTRIGGER_VERSION' ) ) {
 
 define( 'MWSTAKE_MEDIAWIKI_COMPONENT_RUNJOBSTRIGGER_VERSION', '1.0.0' );
 
-$GLOBALS['mwsgRunJobsTriggerOptions'] = [
-	"*" => [
-		"basetime" => [ 1, 0, 0 ],
-		"once-a-week-day" => "sunday"
-	]
-];
+MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
+->register( 'runjobstrigger', function () {
+	$GLOBALS['mwsgRunJobsTriggerOptions'] = [
+		"*" => [
+			"basetime" => [ 1, 0, 0 ],
+			"once-a-week-day" => "sunday"
+		]
+	];
 
-$GLOBALS['mwsgRunJobsTriggerHandlerRegistry'] = [];
+	$GLOBALS['mwsgRunJobsTriggerHandlerRegistry'] = [];
 
-$GLOBALS['mwsgRunJobsTriggerHandlerFactories'] = [
-	'globalvars-config' => [
-		'class' => "\\MWStake\\MediaWiki\\Component\\RunJobsTrigger\\HandlerFactory\\GlobalVars"
-	],
-	'hook' => [
-		'class' => "\\MWStake\\MediaWiki\\Component\\RunJobsTrigger\\HandlerFactory\\Hook",
-		'services' => [ 'MainConfig' ]
-	]
-];
+	$GLOBALS['mwsgRunJobsTriggerHandlerFactories'] = [
+		'globalvars-config' => [
+			'class' => "\\MWStake\\MediaWiki\\Component\\RunJobsTrigger\\HandlerFactory\\GlobalVars"
+		],
+		'hook' => [
+			'class' => "\\MWStake\\MediaWiki\\Component\\RunJobsTrigger\\HandlerFactory\\Hook",
+			'services' => [ 'MainConfig' ]
+		]
+	];
 
-$GLOBALS['mwsgRunJobsTriggerRunnerWorkingDir'] = sys_get_temp_dir();
+	$GLOBALS['mwsgRunJobsTriggerRunnerWorkingDir'] = sys_get_temp_dir();
 
-$GLOBALS['wgJobClasses']['invokeRunner']
-	= "\\MWStake\\MediaWiki\\Component\\RunJobsTrigger\\Job\\InvokeRunner";
+	$GLOBALS['wgJobClasses']['invokeRunner']
+		= "\\MWStake\\MediaWiki\\Component\\RunJobsTrigger\\Job\\InvokeRunner";
 
-$GLOBALS['wgExtensionFunctions'][]
-	= "\\MWStake\\MediaWiki\\Component\\RunJobsTrigger\\Runner::runDeferred";
+	$GLOBALS['wgExtensionFunctions'][]
+		= "\\MWStake\\MediaWiki\\Component\\RunJobsTrigger\\Runner::runDeferred";
 
-$GLOBALS['wgHooks']['LoadExtensionSchemaUpdates'][] = "\\MWStake\\MediaWiki\\Component\\"
-	. "RunJobsTrigger\\Hook\\LoadExtensionSchemaUpdates\\ClearJSONFiles::callback";
+	$GLOBALS['wgHooks']['LoadExtensionSchemaUpdates'][] = "\\MWStake\\MediaWiki\\Component\\"
+		. "RunJobsTrigger\\Hook\\LoadExtensionSchemaUpdates\\ClearJSONFiles::callback";
+} );
